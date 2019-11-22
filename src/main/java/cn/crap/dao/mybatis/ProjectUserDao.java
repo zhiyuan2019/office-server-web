4f55abe65cf2e8809c0f38e9f5521f58746da869
zhiyuan2019 <18218546112@163.com>
2019-11-22 13:32:47 +0800

    update

---
+++ src/main/java/cn/crap/dao/mybatis/ProjectUserDao.java
@@ -0,0 +1,7 @@
+package cn.crap.dao.mybatis;
+
+import cn.crap.model.ProjectUserPO;
+import cn.crap.query.ProjectUserQuery;
+
+public interface ProjectUserDao extends NewBaseDao<ProjectUserPO, ProjectUserQuery> {
+}
---
+++ src/main/java/cn/crap/utils/IAttributeConst.java
@@ -0,0 +1,14 @@
+package cn.crap.utils;
+
+/**
+ * @author Ehsan
+ * @date 2018/12/16 21:37
+ */
+public interface IAttributeConst {
+    String TRUE = "1";
+    String MARK_DOWN = "markdown";
+    /**
+     * 项目环境
+     */
+    String ENV_URL = "envUrl";
+}
